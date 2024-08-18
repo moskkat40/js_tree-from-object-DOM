6ef4231d010e3c640fae4cabcca47df0543c7d8e
Kateryna <moskkat40@gmail.com>
2024-08-18 14:17:53 +0200

    make build

---
+++ index.a89b2c8a.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");c.textContent=a,0!==Object.keys(t[a]).length&&e(c,t[a]),r.append(c)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.a89b2c8a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    element.append(list);
+    for(const key in data){
+        const listElement = document.createElement("li");
+        listElement.textContent = key;
+        if (Object.keys(data[key]).length !== 0) createTree(listElement, data[key]);
+        list.append(listElement);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
